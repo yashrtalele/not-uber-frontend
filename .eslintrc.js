8d11bd69423da7563ab0d6bb08965374a161e7bd
Yash Talele <yashrtalele@gmail.com>
2024-12-13 16:46:33 +0530

    Changed es lint and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
   extends: [
-    "expo",
-    "prettier",
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:prettier/recommended",
+    'expo',
+    'prettier',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["/dist/*"],
-  plugins: ["prettier"],
+  ignorePatterns: ['/dist/*'],
+  plugins: ['prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'import/prefer-default-export': 'off',
   },
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
   },
 };
