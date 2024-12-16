183e0fe03db7a11cc3f01c10e59d921f907a7419
Yash Talele <yashrtalele@gmail.com>
2024-12-16 15:41:20 +0530

    Changed layout and added Default Home and Signin Pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,58 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { View } from 'react-native';
 import 'react-native-reanimated';
-
+import { TamaguiProvider } from 'tamagui';
+import { Slot, useRouter } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { tamaguiConfig } from '../tamagui.config';
+import { AuthProvider, useAuth } from './context/AuthContext';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
+
 SplashScreen.preventAutoHideAsync();
+
+function MainLayout() {
+  const { authState } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      if (authState?.authenticated === false) {
+        router.replace('/(auth)/Signin');
+      } else {
+        router.replace('/(app)/Home');
+      }
+    };
+    checkAuthentication();
+  }, [authState, router]);
+
+  return (
+    <View className="h-full">
+      <Slot />
+    </View>
+  );
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    LexendBlack: require('../assets/fonts/lexend/Lexend-Black.ttf'),
+    LexendBold: require('../assets/fonts/lexend/Lexend-Bold.ttf'),
+    LexendExtraBold: require('../assets/fonts/lexend/Lexend-ExtraBold.ttf'),
+    LexendExtraLight: require('../assets/fonts/lexend/Lexend-ExtraLight.ttf'),
+    LexendLight: require('../assets/fonts/lexend/Lexend-Light.ttf'),
+    LexendMedium: require('../assets/fonts/lexend/Lexend-Medium.ttf'),
+    LexendRegular: require('../assets/fonts/lexend/Lexend-Regular.ttf'),
+    LexendSemiBold: require('../assets/fonts/lexend/Lexend-SemiBold.ttf'),
+    LexendThin: require('../assets/fonts/lexend/Lexend-Thin.ttf'),
   });
 
   useEffect(() => {
@@ -28,12 +66,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <AuthProvider>
+      <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+        <ThemeProvider
+          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <MainLayout />
+          <StatusBar style={colorScheme === 'dark' ? 'light' : 'auto'} />
+        </ThemeProvider>
+      </TamaguiProvider>
+    </AuthProvider>
   );
 }
